726366651709ee41e2093d09df4516123a771ab5
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-11 18:49:27 +0800

    Add exercise3

---
+++ exercise3/exercise3.js
@@ -0,0 +1,16 @@
+loadData('./data.json');
+
+/*
+  透過 jQuery 載入資料
+*/
+function loadData(path) {
+  // Write your code here...
+}
+
+/*
+  將取得的 url 資料，透過 jQuery 在 class 為 ".container" 的元素中
+  加入新的圖片設定 src 且 class="img"
+*/
+function appendImgsFromData(data) {
+  // Write your code here...
+}
